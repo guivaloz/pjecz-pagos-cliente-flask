599a0431712e915b83a6309a94375fbd3904f43b
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-02-05 17:01:11 -0600

    Correccion

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ API_BASE_URL = os.environ.get("API_BASE_URL", "http://localhost:8000/v3")
 API_TIMEOUT = int(os.environ.get("API_TIMEOUT", 12))
 
 # Base URL
-BASE_URL = os.environ.get("API_BASE_URL", "http://localhost:5000")
+BASE_URL = os.environ.get("BASE_URL", "http://localhost:5000")
 
 # Salt para convertir/reconverir el id en hash
 SALT = os.environ.get("SALT", "Esta es una muy mala cadena aleatoria")
